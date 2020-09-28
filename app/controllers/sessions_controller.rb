413afab3916c4f0cda64ba7d821154b377e1c034
Caleb LeNoir <calebl@users.noreply.github.com>
2020-09-28 10:08:55 -0400

    don't authenticate api post to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  protect_from_forgery with: :exception, if: Proc.new { |c| c.request.format != 'application/json' }
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
   before_action :set_session, only: [:show, :edit, :update, :destroy]
 
   # GET /sessions
