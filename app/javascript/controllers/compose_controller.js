0927362944c7df9c5481a9e4ab6147f8007be56e
Caleb LeNoir <calebl@users.noreply.github.com>
2020-10-04 20:41:04 -0400

    js add text and then save, WIP on word count

--- app/javascript/controllers/compose_controller.js
+++ app/javascript/controllers/compose_controller.js
@@ -2,14 +2,43 @@ import { Controller } from "stimulus";
 import $ from "jquery";
 
 export default class extends Controller {
-    static targets = [ "text" ];
+    static targets = [ "text", "count" ];
 
     submit(event) {
+        if(event.key === " " || event.keyCode === 32){
+            console.log($(this.countTarget).text());
+            console.log(parseInt($(this.countTarget).text()))
+        }
+
         if(event.key === "Enter" || event.keyCode === 13){
             event.preventDefault();
 
-            if( this.textTarget.value === "") return;
-            $(document).find(event.target).closest('form').submit();
+            const {textTarget} = this;
+
+            if( textTarget.value === "") return;
+            // $(document).find(event.target).closest('form').submit();
+
+            $(document).find('.text-container .text').append(`<div>${textTarget.value}</div>`)
+
+            //TODO: submit
+            let formData = new FormData();
+            formData.append("session[text]", textTarget.value);
+
+            textTarget.value = '';
+
+            fetch($(this.element).data('session-update-url'), {
+                body: formData,
+                method: 'PUT',
+                dataType: 'script',
+                credentials: "include",
+                headers: {
+                    "X-CSRF-Token": document.getElementsByName('csrf-token')[0].content
+                }
+            })
+                .then((response) => {
+                    console.log(response);
+
+                })
         }
     }
 }
