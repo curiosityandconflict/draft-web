7d22051e86b1fd8ef2f1c795c9f1a549492c4ca6
Caleb LeNoir <1080797+calebl@users.noreply.github.com>
2021-04-14 06:49:58 -0400

    correct email credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'support@curiosityandconflict.com'
+  config.mailer_sender = 'support@write-draft.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
