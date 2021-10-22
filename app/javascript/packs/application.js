2099a94d63d6bd0d3b3c0beeef3c3aa8619ec9b4
Caleb LeNoir <1080797+calebl@users.noreply.github.com>
2021-10-22 06:58:52 -0400

    remove trix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "@hotwired/turbo-rails"
-import "trix"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
