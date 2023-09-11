5f192addcd3d82f7440f65429a5c5b16cbae8893
Caleb LeNoir <1080797+calebl@users.noreply.github.com>
2023-09-11 10:39:06 -0400

    try headless

--- test/system/application_system_test.rb
+++ test/system/application_system_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome
   include Devise::Test::IntegrationHelpers
 
   setup do
